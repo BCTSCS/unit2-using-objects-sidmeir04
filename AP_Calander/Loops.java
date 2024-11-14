54febc785dc0ea5afd944beaf25ae2e9f30dd5a0
sidmeir04 <oscjep25@gmail.com>
2024-11-14 14:14:21 -0500

    loops

--- AP_Calander/Loops.java
+++ AP_Calander/Loops.java
@@ -2,15 +2,43 @@ package AP_Calander;
 public class Loops {
 
     public static void printNums(int start, int end, int increment){
+        System.out.println("Start: " + start + " / Stop: " + end + " / " + "increment: " + increment );
         for (int i = start; i<end; i += increment){
             System.out.print(i + " ");
         }
         System.out.println("");
     }
 
+    public static void sumNums(String nums){
+        int sum = 0;
+        int index = 0;
+
+        while (index < nums.length()) {
+            sum += Character.getNumericValue(nums.charAt(index));
+            index++;
+        }
+        System.out.println(nums.length() + " digits totaling " + sum);
+    }
+
+    public static void addHyphen(String input) {
+        String uppercased = input.toUpperCase();
+        System.out.println("Uppercase string: " + uppercased);
+        String hyphenated = uppercased.replace(" ", "-");
+        System.out.println("Hyphenated string: " + hyphenated);
+    }
+
     public static void main(String[] args) {
         printNums(2, 90, 5);
         printNums(-10, 8, 2);
         printNums(100, 150, 5);
+        sumNums("234");
+        sumNums("10000");
+        sumNums("123456789");
+        sumNums("222222");
+        addHyphen("Hello World");
+        addHyphen("Jim Bob");
+        addHyphen("Computer Science");
+        addHyphen("UIL TCEA");
+        addHyphen("State Champions");
     }
 }
